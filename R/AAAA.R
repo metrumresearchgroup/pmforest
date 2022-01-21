e34cd7e3fa9c9c129839d310c928efceecab61d0
Kyle Barrett <barrettk@metrumrg.com>
2022-01-21 13:36:00 -0500

    init branch (Namespace changes)

--- R/AAAA.R
+++ R/AAAA.R
@@ -4,11 +4,11 @@
 #' @importFrom readr read_table read_csv
 #' @importFrom tibble as_tibble tibble
 #' @importFrom pmtables sig
-#' @importFrom dplyr summarise mutate group_by filter select left_join rename if_else distinct n_distinct all_equal
 #' @import rlang
 #' @import dplyr
 #' @import ggplot2
 #' @import patchwork
+#' @import vdiffr
 #' @importFrom stringr str_detect
 #' @importFrom utils write.csv
 #' @importFrom assertthat assert_that
