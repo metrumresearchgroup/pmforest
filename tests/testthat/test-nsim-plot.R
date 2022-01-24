3e475214c87f0449892dd81a61eba89ac594c72b
Kyle Barrett <barrettk@metrumrg.com>
2022-01-24 10:57:33 -0500

    nsim tests

--- tests/testthat/test-nsim-plot.R
+++ tests/testthat/test-nsim-plot.R
@@ -1,3 +1,5 @@
+library(vdiffr)
+
 specDir <- here::here("tests",  "test-data", "spec")
 dataDir <- here::here("tests",  "test-data")
 
@@ -10,57 +12,57 @@ all_labels$V2 <- 'V (L)' # uncomment this line if using V2
 
 plot_labels <- as_labeller(unlist(all_labels))
 
-describe("Multiple simulations [PMF-PLOT-012]", {
+describe("Multiple simulations", {
 
-  plotData <- readRDS(file.path(dataDir, "plotDataEXP.RDS"))
 
-  plt <- plot_forest(data = plotData,
-                     statistic = "mean",
-                     CI=0.95,
-                     stat = stat,
-                     covariate = GROUP,
-                     nsim = nsim,
-                     cov_level = LVL,
-                     shaded_interval = c(0.8,1.25),
-                     summary_label = plot_labels,
-                     text_size = 3.5,
-                     vline_intercept = 1,
-                     x_lab = "Fraction and 95% CI \nRelative to Reference",
-                     CI_label = "Median [95% CI]"
-  )
+  it("Multiple simulations base test [PMF-PLOT-012]", {
 
-  plt
+    plotData <- readRDS(file.path(dataDir, "plotDataEXP.RDS"))
 
+    plt <- plot_forest(data = plotData,
+                       statistic = "mean",
+                       CI=0.95,
+                       stat = stat,
+                       covariate = GROUP,
+                       nsim = nsim,
+                       cov_level = LVL,
+                       shaded_interval = c(0.8,1.25),
+                       summary_label = plot_labels,
+                       text_size = 3.5,
+                       vline_intercept = 1,
+                       x_lab = "Fraction and 95% CI \nRelative to Reference",
+                       CI_label = "Median [95% CI]"
+    )
+    plt
+    expect_doppelganger("Multiple simulations", plt)
+  })
+
+
+
+
+  it("Multiple CI's with jitter [PMF-PLOT-013]", {
+
+    plotData <- readRDS(file.path(dataDir, "plotDataEXP.RDS")) %>% mutate(stat = stat + (nsim/300))
+
+    plt <- plot_forest(data = plotData,
+                       statistic = "mean",
+                       CI=0.95,
+                       stat = stat,
+                       covariate = GROUP,
+                       metagroup = param,
+                       nsim = nsim,
+                       cov_level = LVL,
+                       shaded_interval = c(0.8,1.25),
+                       summary_label = plot_labels,
+                       text_size = 4,
+                       vline_intercept = 1,
+                       x_lab = "Fraction and 95% CI \nRelative to Reference",
+                       CI_label = "Median [95% CI]",
+                       jitter_nsim = TRUE)
+
+    plt
+    expect_doppelganger("Multiple CI's with jitter", plt)
+  })
 
 })
-
-
-
-
-describe("Multiple CI's with jitter [PMF-PLOT-013]", {
-
-  plotData <- readRDS(file.path(dataDir, "plotDataEXP.RDS")) %>% mutate(stat = stat + (nsim/300))
-
-  plt <- plot_forest(data = plotData,
-                     statistic = "mean",
-                     CI=0.95,
-                     stat = stat,
-                     covariate = GROUP,
-                     metagroup = param,
-                     nsim = nsim,
-                     cov_level = LVL,
-                     shaded_interval = c(0.8,1.25),
-                     summary_label = plot_labels,
-                     text_size = 4,
-                     vline_intercept = 1,
-                     x_lab = "Fraction and 95% CI \nRelative to Reference",
-                     CI_label = "Median [95% CI]",
-                     jitter_nsim = TRUE)
-
-  plt
-
-
-})
-
-
 
