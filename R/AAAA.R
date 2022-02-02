34364cca72405e9ca5f11c2c263b49448f2f23e8
seth127 <seth127@gmail.com>
2022-02-02 10:28:58 -0500

    chore: updating DESCRIPTION, pkgr.yml, and AAAA.R to clean up dependencies

--- R/AAAA.R
+++ R/AAAA.R
@@ -2,18 +2,15 @@
 #' @importFrom here here
 #' @importFrom readr read_table read_csv
 #' @importFrom tibble as_tibble tibble
-#' @importFrom pmtables sig
 #' @import rlang
 #' @import dplyr
 #' @import ggplot2
 #' @importFrom gridExtra arrangeGrob
 #' @importFrom ggpubr as_ggplot
 #' @import patchwork
-#' @import vdiffr
 #' @importFrom stringr str_detect
 #' @importFrom utils write.csv
 #' @importFrom assertthat assert_that
-#' @importFrom yspec ys_load ys_get_short
 #'
 #' @include forest-constructor.R
 #' @include table-plot.R
