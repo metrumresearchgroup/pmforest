98538adcb085b3e4848cfb28064b0c94319fe80f
barrettk <barrettk@metrumrg.com>
2023-05-25 12:31:51 -0400

    Added two new arguments:  - CI_bracket_open, CI_bracket_close  - these arguments allow users to set the format of the open and closing interval (brackets or parenthesis)
    Changed warning to a message for extra columns
     - Think this could be somewhat of a common occurence, and dont think it deserves a warning since it doesn't impact anything

--- tests/testthat/test-input-data.R
+++ tests/testthat/test-input-data.R
@@ -33,7 +33,7 @@ test_that("plot_forest() input data check warns with extra columns [PMF-DATA-004
       hi = c(5, 6, 7),
       naw = c(7, 8, 9)
     )
-  expect_warning(
+  expect_message(
     plt <- plot_forest(df),
     "has extra columns"
   )
